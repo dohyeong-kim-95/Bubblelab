aa29bbbcd2ea3da0f3281e2630e2f4ff4cd9514b
Kim Dohyeong <167014279+dohyeong-kim-95@users.noreply.github.com>
2026-07-27 09:11:44 +0900

    slop: 분류 게임 기록 등록

--- _infra/records.js
+++ _infra/records.js
@@ -33,6 +33,7 @@ export const GAMES = {
   flags:        { dir: "max", min: 0, max: 1000 },      // 연속으로 맞춘 국기 수
   logroll:      { dir: "max", min: 0, max: 100000 },    // 버틴 시간(초)
   reactiontime: { dir: "min", min: 0, max: 60000 },     // 반응속도(ms)
+  "sort-line":  { dir: "max", min: 0, max: 100000 },    // 컨베이어 분류 정답 수
   touch25:      { dir: "min", min: 0, max: 3600 },      // 완주 시간(초)
   trader:       { dir: "max", min: -1, max: 100 },      // 수익률(비율)
   woodstack:    { dir: "max", min: 0, max: 100000 },    // 쌓은 통나무 층수
